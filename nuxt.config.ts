7059f5082073ff130f71959a502800f36d8e1ec0
M.Spinks <mikespinks0401@gmail.com>
2022-09-23 17:50:24 -0500

    added pinia with auto imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,26 @@
+import { acceptHMRUpdate } from "pinia";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    head: {
-        link: [
-            {rel: 'icon', type: 'image/x-icon', href:"/favicon.ico"}
-        ]
-    },
-    modules: [
-        "@nuxtjs/tailwindcss"
-    ]
+  head: {
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          'defineStore', // import { defineStore } from 'pinia'
+          'acceptHMRUpdate', // import hot model updates
+          // automatically imports `defineStore` as `definePiniaStore`
+          ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+      },
+    ],
+  ],
+  imports: {
+    dirs: ['stores'],
+  },
 })
