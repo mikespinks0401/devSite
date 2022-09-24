bb7a511b7278a357515dcd0f5f17069eca0b8293
M.Spinks <mikespinks0401@gmail.com>
2022-09-24 14:32:14 -0500

    added vite server config for vue component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "cypress";
-
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
+      viteConfig:{
+        plugins: [
+          vue()
+        ]
+      }
     },
   },
 });
