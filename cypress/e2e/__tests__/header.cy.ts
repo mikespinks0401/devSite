03577ba30626e1864bd30122ccc5db1772030aab
M.Spinks <mikespinks0401@gmail.com>
2022-09-24 14:35:06 -0500

    formatted header e2e test file

--- cypress/e2e/__tests__/header.cy.ts
+++ cypress/e2e/__tests__/header.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('The Header', () => {
-  beforeEach(()=>{
+  beforeEach(() => {
     cy.visit('/')
   })
   context('Nav Bar Renders on mobile screens', () => {
@@ -13,44 +13,45 @@ describe('The Header', () => {
       cy.get('[data-cy="hamburger"]')
     })
 
-    it('aria-Expanded false when loaded', ()=>{
-        
-        cy.get('[data-cy="hamburger"]').should( ($menu) => {
-         
-            const isExpanded = $menu.attr('aria-expanded')
-            assert.equal(isExpanded, "false"  , "Menu Should not be expanded")
-        })
+    it('aria-Expanded false when loaded', () => {
+      cy.get('[data-cy="hamburger"]').should(($menu) => {
+        const isExpanded = $menu.attr('aria-expanded')
+        assert.equal(isExpanded, 'false', 'Menu Should not be expanded')
+      })
     })
 
-    it('aria-Expanded true when hamburger icon clicked', ()=>{
-
-        // cy.get('[data-cy="hamburger"]').click().should('have.attr', 'aria-expanded', 'true')
-        // cy.get('[data-cy="hamburger"]').click()
-        cy.get('[data-cy="hamburger"]').wait(50).click().should('have.attr', 'aria-expanded', 'true')
+    it('aria-Expanded true when hamburger icon clicked', () => {
+      cy.get('[data-cy="hamburger"]')
+        .click()
+        .should('have.attr', 'aria-expanded', 'true')
     })
 
     it('contains logo', () => {
       cy.get('[data-cy="logo"]')
     })
 
-    it('mobile menu should be hidden by default', ()=>{
+    it('mobile menu should be hidden by default', () => {
       cy.get('[data-cy="mobile menu"]').should('not.be.visible')
     })
 
-    it('mobile menu visible when clicked', ()=>{
-      cy.visit('/')
+    it('mobile menu visible when clicked', () => {
       cy.get('[data-cy="hamburger"]').wait(50).click()
       cy.get('[data-cy="mobile menu"]').should('be.visible')
     })
-  })
 
-  context('Properly renders on medium screens', ()=>{
-    before(()=>{
-      cy.viewport("ipad-2")
-    })
-    it('does not contain hamburger menu', ()=>{
-      cy.get('[data-cy="hamburger"').should('not.be.visible')
+    it('Mobile menu closes when A Button / Page is clicked', () => {
+      cy.get('[data-cy="hamburger"]').wait(50).click()
+      cy.get('[data-cy="mobile menu"]').should('be.visible')
+      cy.get('[data-cy="navList"] li').should('have.length.above', 1)
     })
   })
 
+  context('Properly renders on medium screens', () => {
+    before(() => {
+      cy.viewport('ipad-2')
+    })
+    it('does not contain hamburger menu', () => {
+      cy.get('[data-cy="hamburger"]').should('not.be.visible')
+    })
+  })
 })
