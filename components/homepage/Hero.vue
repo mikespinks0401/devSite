52476773bfdf4de5db56d0dcd9ad55c3c388cd11
M.Spinks <mikespinks0401@gmail.com>
2022-09-26 15:10:49 -0500

    Header-Hero Created

---
+++ components/homepage/Hero.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="container mx-auto grid md:grid-cols-2 bg-green-200">
+      <div class="mt-10 ml-10 container w-full">
+        <div>
+          <h1 class="text-6xl leading-tight font-semibold">Hello,</h1>
+          <h2 class="text-2xl">My Name is <span class="text-primaryAccent font-medium">Michael Spinks</span></h2>
+          <p>I am a</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
