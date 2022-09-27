9d569194292d429bbb46cb52f937a2252ee4a6bd
M.Spinks <mikespinks0401@gmail.com>
2022-09-27 16:09:07 -0500

    added colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
         primaryDark: '#1C2230',
         primaryAccent: '#EA652C',
         primaryAccentDark: '#FCA47C',
+        primaryText: 'rgb(17, 24, 39)',
+        primaryTextDark: 'rgb(243, 244, 246)'
       },
     },
   },
