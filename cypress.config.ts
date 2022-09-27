75d0afef86503b01303db3ef65a6d72ee321a3e5
M.Spinks <mikespinks0401@gmail.com>
2022-09-27 16:09:38 -0500

    added video:false in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+  video: false,
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -10,13 +11,11 @@ export default defineConfig({
 
   component: {
     devServer: {
-      framework: "vue",
-      bundler: "vite",
-      viteConfig:{
-        plugins: [
-          vue()
-        ]
-      }
+      framework: 'vue',
+      bundler: 'vite',
+      viteConfig: {
+        plugins: [vue()],
+      },
     },
   },
-});
+})
