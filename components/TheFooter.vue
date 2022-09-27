ef2c4012bb115df8bafa655762ad47fe63bdf75b
M.Spinks <mikespinks0401@gmail.com>
2022-09-27 10:32:29 -0500

    Open links in external window

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -10,16 +10,17 @@
 </script>
 
 <template>
-  <div data-cy="Footer" class="p-3 w-full border-t">
+  <div
+    data-cy="Footer"
+    class="p-3 w-full border-t dark:border-t-gray-500 dark:bg-black dark:text-white transition"
+  >
     <div class="container max-w-7xl mx-auto h-full">
-      <div
-        class="flex flex-col gap-2 h-full items-center justify-between"
-      >
+      <div class="flex flex-col gap-2 h-full items-center justify-between">
         <div class="px-2 flex h-full justify-center items-center gap-1">
           <nuxt-link
             to="https://github.com/mikespinks0401"
-            alt="Go To My Github Profile"
             aria-describedby="link to Github profile"
+            target="_blank"
           >
             <logos-github :size="20" />
           </nuxt-link>
@@ -27,6 +28,7 @@
             to="https://www.linkedin.com/in/michael-spinks-254879201"
             alt="Go To My Linked In Profile"
             aria-describedby="link to linkedIn profile"
+            target="_blank"
           >
             <logos-linked-in :size="20" />
           </nuxt-link>
