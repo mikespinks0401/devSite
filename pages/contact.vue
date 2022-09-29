129a470880d9c82c43dc8684fb13a2ff63afb61a
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 15:08:27 -0500

    Created Contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,29 @@
-<script setup lang='ts'>
-
-</script>
+<script setup lang="ts"></script>
 <template>
-    <div>
-
+  <div class="flex-1">
+    <div class="flex justify-center mt-6">
+      <h1 class="text-5xl text-opacity-80">Contact</h1>
     </div>
+    <div class="container w-full mx-auto grid lg:grid-cols-4 mt-20">
+      <div data-cy="contact-left" class="md:col-span-3 px-4">
+        <p class="text-2xl font-semibold text-primaryAccent">
+          Ready To Collaborate?
+        </p>
+        <h2 class="text-3xl pt-5">Let's get to work</h2>
+        <p class="pt-5">
+          I look forward to connecting with you and will respond within 48 hours
+        </p>
+        <br />
+        <p>Thank you for stopping by!</p>
+        <contact-form />
+      </div>
+        <div data-cy="contact-info" class="w-full flex flex-col justify-center items-center">
+        
+          <contact-info-card>
+             <contact-info-card-item /> 
+          </contact-info-card>
+        </div>
+      </div>
+    </div>
+  
 </template>
