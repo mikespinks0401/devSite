7d04b3b30f764dc4b72934a9c2e10c5f49bcbd20
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 17:36:34 -0500

    cleaned up spacing

--- components/icons/offCenterIcon.vue
+++ components/icons/offCenterIcon.vue
@@ -6,16 +6,14 @@
   })
   const bgColor = props.bgColor
   const iconBgClass = `bg-${bgColor}`
-
 </script>
 
 <template>
-    <div class="pr-12">
-      <div class="h-4 w-4 bg-primaryAccent">
-        <div class="relative top-[6px] left-2">
-          <icons-contact-icons :icon-name="icon" :size="props.size"/>
-        </div>
+  <div class="pr-12">
+    <div class="h-4 w-4 bg-primaryAccent">
+      <div class="relative top-[6px] left-2">
+        <icons-contact-icons :icon-name="icon" :size="props.size"/>
       </div>
     </div>
-
+  </div>
 </template>
