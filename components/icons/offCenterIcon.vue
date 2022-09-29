2f3e125fee75a461d3efbb777ad5b2c906dd7a96
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 17:17:26 -0500

    component to wrap contact-Icons for more control

---
+++ components/icons/offCenterIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const props = defineProps({
+    size: Number,
+    icon: String,
+    bgColor: String,
+  })
+  const bgColor = props.bgColor
+  const iconBgClass = `bg-${bgColor}`
+
+</script>
+
+<template>
+    <div class="pr-12">
+      <div class="h-4 w-4 bg-primaryAccent">
+        <div class="relative top-[6px] left-2">
+          <icons-contact-icons :icon-name="icon" :size="props.size"/>
+        </div>
+      </div>
+    </div>
+
+</template>
