dddda79c29d28cb471a91342e5646b37cd9e37e5
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 17:36:54 -0500

    cleaned up spacing

--- components/contact/InfoCardItem.vue
+++ components/contact/InfoCardItem.vue
@@ -1,33 +1,26 @@
 <script setup lang="ts">
-    const props = defineProps({
-        itemTitle: String,
-        itemIcon: String,
-        itemDescriptions: Array<string>
-    })
-    const icon = props.itemIcon
-    const title = props.itemTitle
-    const descriptions = props.itemDescriptions
-    
+  const props = defineProps({
+    itemTitle: String,
+    itemIcon: String,
+    itemDescriptions: Array<string>,
+  })
+  const icon = props.itemIcon
+  const title = props.itemTitle
+  const descriptions = props.itemDescriptions
 </script>
 
 <template>
   <div class="flex">
-    <div class="pr-12">
-      <div class="h-4 w-4 bg-primaryAccent">
-        <div class="relative top-[6px] left-2">
-        <icons-contact-icons :icon-name="icon"/>
-        </div>
-      </div>
-    </div>
+    <icons-off-center-icon :icon="icon" :size="24" />
     <div>
-      <h3 class="font-bold text-xl text-primaryAccent">{{props.itemTitle}}</h3>
-        <ul>
-          <li v-for="(item, index) in descriptions" :key="index">
-            <p class="italic font-semibold text-lg">{{item}}</p>
-          </li>
-        </ul>
-
-        
+      <h3 class="font-bold text-xl text-primaryAccent">
+        {{ props.itemTitle }}
+      </h3>
+      <ul>
+        <li v-for="(item, index) in descriptions" :key="index">
+          <p class="italic font-semibold text-lg">{{ item }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
