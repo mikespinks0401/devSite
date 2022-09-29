d6ec4753efe741c4db7efa7677ab3e2fbbd511e4
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 15:08:02 -0500

    added div for modal to teleport to on layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col min-h-screen w-full">
+  <div class="flex flex-col min-h-screen w-full relative">
+    <div id="modal"></div>
     <the-header />
-    <div class="flex-1 dark:bg-gray-900 dark:text-gray-100 transition">
-      <slot/>
+    <div
+      class="flex flex-col flex-1 dark:bg-gray-900 dark:text-gray-100 transition"
+    >
+      <slot />
     </div>
     <the-footer />
   </div>
