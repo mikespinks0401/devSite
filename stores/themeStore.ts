24f9f3c834a66b9e3e86178862692ea5f071f881
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 15:03:11 -0500

    create theme init - grab sys theme preference

--- stores/themeStore.ts
+++ stores/themeStore.ts
@@ -1,10 +1,21 @@
 export const useThemeStore = defineStore('themeStore', () => {
   const isLightMode = ref(true)
 
-
+  function init() {
+    return new Promise((resolve, reject) => {
+      const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)')
+      if (isDarkTheme.matches) {
+        isLightMode.value = false
+        resolve(true)
+      } else {
+        console.log('dark mode off')
+        resolve(false)
+      }
+    })
+  }
   function changeLightMode() {
     isLightMode.value = !isLightMode.value
   }
 
-  return { isLightMode, changeLightMode }
+  return { isLightMode, changeLightMode, init }
 })
