9bfe17663e385cfe347457c6b951c61efdb1a2e3
M.Spinks <mikespinks0401@gmail.com>
2022-09-30 10:00:36 -0500

    added ref to account for mobile menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const navBarOpen = ref(false)
+  const bodyMarginTopClass = computed(() => {
+    return navBarOpen.value === true ? ' ' : 'mt-10 md:mt-12'
+  })
+</script>
 
 <template>
   <div class="flex flex-col min-h-screen w-full relative">
     <div id="modal"></div>
     <the-header />
     <div
-      class="mt-10 md:mt-12 flex flex-col flex-1 bg-slate-50 dark:bg-gray-900 dark:text-gray-100 transition"
+      class="flex flex-col flex-1 bg-slate-50 dark:bg-gray-900 dark:text-gray-100 transition"
+      :class="bodyMarginTopClass"
     >
       <slot />
     </div>
     <the-footer />
   </div>
 </template>
+<style>
+
+</style>
