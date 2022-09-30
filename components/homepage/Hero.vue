62182a3fd35c08921c57b3ba9d41fe511ea57b3c
M.Spinks <mikespinks0401@gmail.com>
2022-09-30 09:59:52 -0500

    added image to hero section

--- components/homepage/Hero.vue
+++ components/homepage/Hero.vue
@@ -13,29 +13,59 @@
 </script>
 
 <template>
-  <div class="w-full " data-cy="hero">
-    <div class="max-w-7xl mx-auto grid md:grid-cols-2">
+  <div class="flex-1 flex flex-col w-full overflow-hidden" data-cy="hero">
+    <div
+      class="flex-1 w-full container max-w-6xl mx-auto grid md:grid-cols-2 p-4 md:px-2"
+    >
       <div
-        class=" dark:border-color-white border-color-black h-96 flex flex-col justify-center gap-1 p-4 tracking-widest"
         data-cy="hero-left"
+        class="flex flex-col justify-center gap-2 md:p-4 tracking-widest"
       >
-        <h1 class="text-4xl leading-tight font-medium mt-4">
-          <span class="text-7xl font">Hello,</span><br />
+        <h1 class="text-3xl font-regular text-gray-800 dark:text-gray-100">
+          <span class="">Hello,</span>
           My Name is
-          <span class="text-primaryAccent font-bold text block md:inline-block">{{
-            props.name
-          }}</span>
+          <span
+            class="text-primaryAccent text-5xl font-bold text block md:inline-block"
+            >{{ props.name }}</span
+          >
         </h1>
-        <p class="text-3xl">
-          I am a 
-          <span class="font-semibold block md:inline-block">
-            <homepage-text-writer :titles="titles" :loop='false'/>
+        <p class="text-3xl -m-1">
+          I am a
+          <span class="block text-4xl font-semibold md:inline-block">
+            <homepage-text-writer
+              :titles="titles"
+              :loop="false"
+              :timeToStart="800"
+              :pauseTime="900"
+            />
           </span>
         </p>
-        <div class="mt-4">
-        <nuxt-link to="/contact" class="px-4 p-2 bg-primaryAccent2 hover:bg-opacity-90 text-white text-lg font-medium">Get In Touch</nuxt-link>
+        <div class="leading-none">
+          <p class="text-lg opacity-80">
+            I build fast, secure, and user friendly apps
+          </p>
+          <p class="text-lg opacity-80">Have something in mind?</p>
         </div>
-        <div></div>
+        <div class="mt-6">
+          <nuxt-link
+            to="/contact"
+            class="px-4 p-2 bg-primaryAccent2 hover:bg-opacity-90 text-white text-lg font-medium"
+            >Get In Touch</nuxt-link
+          >
+        </div>
+      </div>
+      <div class="flex flex-col w-full" data-cy="hero-right">
+        <div
+          class="bg-gradient-radial from-primaryAccent via-inherit w-full h-full"
+        >
+          <div class="relative">
+            <img
+              class="relative top-15 left-20"
+              src="/heroImg.png"
+              alt="Michael Spinks Hero image"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
