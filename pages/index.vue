24be08c96f9fa5ca5f38aa71857dd8ab765cd8b6
M.Spinks <mikespinks0401@gmail.com>
2022-09-30 10:01:11 -0500

    added services section

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="flex-1 flex flex-col">
-    <homepage-hero 
-    name="Michael Spinks"
-    />
+  <div class="flex-1 flex flex-col mt-4 md:mt-0">
+    <homepage-hero name="Michael Spinks" />
+    <homepage-services />
   </div>
 </template>
