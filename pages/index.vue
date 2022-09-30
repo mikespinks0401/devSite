81a16b0a0d374914468e8f2976c0e9196604d6a2
M.Spinks <mikespinks0401@gmail.com>
2022-09-30 16:34:37 -0500

    move mrg from homePage to layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex-1 flex flex-col mt-4 md:mt-0">
+  <div class="flex-1 flex flex-col">
     <homepage-hero name="Michael Spinks" />
     <homepage-services />
   </div>
