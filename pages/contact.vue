c7130c8b466777e16c32010b6e5e7e36699bfd25
M.Spinks <mikespinks0401@gmail.com>
2022-09-29 21:24:46 -0500

    adjusted h1 position and responsive designs

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="flex-1">
     <div class="flex justify-center mt-6">
-      <h1 class="text-5xl text-opacity-80">Contact</h1>
+      <h1 class="text-5xl text-opacity-80 mb-16">Contact</h1>
     </div>
-    <div class="container w-full mx-auto grid lg:grid-cols-4 mt-20">
+    <div class="container w-full mx-auto grid lg:grid-cols-4 mt-18 gap-16">
       <div data-cy="contact-left" class="md:col-span-3 px-4">
         <p class="text-2xl font-semibold text-primaryAccent">
           Ready To Collaborate?
@@ -17,7 +17,7 @@
         <p>Thank you for stopping by!</p>
         <contact-form />
       </div>
-        <div data-cy="contact-info" class="w-full flex flex-col justify-center items-center">
+        <div data-cy="contact-info" class="w-full flex flex-col justify-center lg:items-center px-4">
         
           <contact-info-card>
              <contact-info-card-item /> 
