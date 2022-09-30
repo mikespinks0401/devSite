dcdc9d5ef6584094d12d7dec08ace7a3a6df9c22
M.Spinks <mikespinks0401@gmail.com>
2022-09-30 09:59:19 -0500

    created skills section

---
+++ components/homepage/Services.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col min-h-screen bg-white dark:bg-black">
+    <div class="flex-1 container max-w-6xl mx-auto">
+      <h2 class="text-4xl text-center mt-8">Skills</h2>
+    </div>
+  </div>
+</template>
