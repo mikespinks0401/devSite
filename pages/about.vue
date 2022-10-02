9d25f164380d99e858c0425dd4ca579e8b821a89
M.Spinks <mikespinks0401@gmail.com>
2022-10-02 14:41:11 -0500

    Fixed some grammatical errors

--- pages/about.vue
+++ pages/about.vue
@@ -18,22 +18,25 @@
         </h2>
         <div class="py-8 md:text-lg">
           <p class="">
-            Welcome, <br />My name is Michael Spinks. I am a self taught developer. My journey began in 2019. My methods of learning have
-            primarily been through books, and online based courses.
+            Welcome, 
+            <br />
+            <br />
+            My name is Michael Spinks. I am a self taught developer. My journey began in 2019. My methods of learning have
+            primarily been through books and online based courses.
           </p>
           <br />
           <p>
-            My journey started with <span class="font-semibold">PHP </span> and
-            <span class="font-semibold">MySQL</span>
+            My journey started with learning<span class="font-semibold">PHP </span> and
+            <span class="font-semibold">MySQL</span>,
             where I learned the fundamentals of programming and had an
-            introduction in how network requests work('get', 'post', 'put',
+            introduction to how network requests work('get', 'post', 'put',
             'patch', etc.) as well as the Object Oriented Programming paradigm.
             With <span class="font-semibold">PHP</span> I learned how to
             interact with databases as well as common vulnerabilites that exist
             when processing data from the client to databases. The most
             important rule that I learned was
             <span class="font-bold">NEVER TRUST USER INPUT</span>. Working with
-            <span class="font-semibold">MySQL</span>, I learned design
+            <span class="font-semibold">MySQL</span>, I learned database design
             principles such as ACID(Atomocity, Consistency, Isolation, and
             Durablity) and database nomalization(each table's columns should be broken down and only contain
             information pertaining to that table).
@@ -46,7 +49,7 @@
               >HTML, CSS, and Vanilla Javascript.</span
             >
             Learning <span class="font-semibold">Vanilla Javascript</span> and <span class="font-semibold">CSS</span> was fundemental before moving on
-            to learning frameworks and libraries.I learned the basic structure
+            to learning frameworks and libraries. I learned the basic structure
             of the DOM(Document Object Model), how to style the DOM with
             <span class="font-semibold">CSS</span>, how to interact with the DOM
             using <span class="font-semibold">Javascript</span>.
@@ -57,9 +60,9 @@
           <p>
             <span class="font-semibold">CSS</span> or
             <span class="font-semibold">Cascading Style Sheet</span> - the
-            markup language necessary to produce beautiful applications. The key
+            markup language necessary to produce beautiful applications. In my time spent soley dedicated to <span class="font-semibold">CSS</span>, the key
             takeaways came down to a few things, the Box Model(Padding, Border,
-            Margin) and creating layouts using (Grid or FlexBox), and Media
+            Margin), layout systems(Grid or FlexBox), and Media
             Queries for responsive applications. I currenly use
             <a
               class="font-semibold"
@@ -77,7 +80,7 @@
             interactivity, it all comes down to JavaScript. The key concepts
             grasped here were Callbacks, Promises, and Event Listeners.
             Understanding these concepts at the core level allows for complete
-            control of what happens on the client side. I have spent some time
+            control of how the application performs on the client side. I had spent some time
             with
             <a
               class="font-semibold"
