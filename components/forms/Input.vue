a06c04fbf6a61549ef7412d1688bcff1f4d191b4
M.Spinks <mikespinks0401@gmail.com>
2022-10-03 08:49:53 -0500

    fixed floating label on click. @click=Focus >input

--- components/forms/Input.vue
+++ components/forms/Input.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import { ref, computed } from 'vue'
   const emit = defineEmits(['updateText'])
   const props = defineProps<{
     fieldName: string
@@ -9,6 +8,7 @@
     showLabel?: boolean
     required?: boolean
   }>()
+  const input = ref(null)
   const textValue = ref('')
   const isFocused = ref(false)
 
@@ -21,22 +21,31 @@
       ? 'text-[0.5rem] translate-y-0  translate-x-1'
       : 'left-0 translate-y-2 translate-x-3'
   })
+
+  const focusInput = () => {
+    isFocused.value = true
+    setTimeout(() => {
+      input.value.focus()
+    }, 200)
+  }
 </script>
 
 <template>
   <div class="relative">
     <label
+      @click="focusInput"
       for="name"
-      class="absolute text-gray-700 transition duration-300 block"
+      class="absolute text-gray-700 transition duration-300 block cursor text z-0 cursor-text"
       :class="focusedClassStyles"
-      >{{ props.fieldName
-      }}<span v-if="props.required" class="text-red-600">*</span></label
-    >
+      >{{ props.fieldName }}
+      <span v-if="props.required" class="text-red-600">*</span>
+    </label>
     <input
+      ref="input"
       @blur="isFocused = false"
       @focus="isFocused = true"
       @input="emit('updateText', textValue), (isFocused = true)"
-      class="w-full focus:ring-0 dark:text-black"
+      class="w-full focus:ring-0 dark:text-black z-50"
       :class="props.classes"
       :type="props.inputType"
       :name="props.fieldName"
