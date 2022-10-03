c3360fbbf0f763de5561fd9afa501386f55dc23a
M.Spinks <mikespinks0401@gmail.com>
2022-10-02 19:10:32 -0500

    Made component for Register / Login Form

---
+++ components/auth/Form.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <form @submit.prevent data-cy="auth form">
+      <forms-input field-name="email" />
+      <forms-input field-name="password" />
+      <forms-input field-name="passwordConfirm" />
+      <input
+        data-cy="formSubmit"
+        class="cursor-pointer"
+        type="submit"
+        aria-label="submit registration"
+        value="Register Now"
+      />
+    </form>
+  </div>
+</template>
