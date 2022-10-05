8229c1743c1a84ec6162c16f2d411ead77c77a93
M.Spinks <mikespinks0401@gmail.com>
2022-10-04 22:30:57 -0500

    Created Form for Registration

--- components/auth/Form.vue
+++ components/auth/Form.vue
@@ -1,22 +1,54 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    handleSubmit: Function
-  }>()
-  const emits = defineEmits(['error'])
-  const email = ref('')
-  const password = ref('')
+const props = defineProps<{
+  handleSubmit?: Function
+}>()
+const emits = defineEmits(['error']['sendFormData'])
 
-  function submitForm() {
-    props.handleSubmit()
-  }
+const userData = reactive({
+  email: '',
+  password: '',
+  passwordConfirm: ''
+})
+
+const updateEmail = (v) => {
+  console.log(userData.email)
+  userData.email = v
+}
+const updatePassword = (v) => {
+  userData.password = v
+}
+const updatePasswordConfirm = (v) => {
+  userData.passwordConfirm = v
+}
+const submitForm = () => {
+  emits('sendFormData', userData)
+}
 </script>
 
 <template>
   <div class="z-40">
-    <form @submit.prevent="submitForm" data-cy="auth form">
-      <forms-input field-name="email" inputType="string" />
-      <forms-input field-name="password" inputType="string" />
-      <forms-input field-name="passwordConfirm" inputType="string" />
+    <form
+      @submit.prevent="submitForm"
+      data-cy="auth form"
+    >
+      <form-component-input
+        @update-text="updateEmail"
+        field-name="email"
+        inputType="string"
+        v-model="userData.email"
+      />
+      <form-component-input
+        @update-text="updatePassword"
+        field-name="password"
+        inputType="string"
+        v-model="userData.password"
+      />
+      <form-component-input
+        @update-text="updatePasswordConfirm"
+        field-name="passwordConfirm"
+        inputType="string"
+        v-model="userData.passwordConfirm"
+      />
       <input
         data-cy="formSubmit"
         class="cursor-pointer"
