e6e374642e47bc3e356767ebb1726caa888f335c
M.Spinks <mikespinks0401@gmail.com>
2022-10-05 12:31:43 -0500

    added form-title prop to form

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex-1 flex flex-col w-full overflow-hidden" data-cy="about">
-    <div class="flex justify-center mt-6">
-      <h1 class="text-5xl text-opacity-80 mb-16">Login</h1>
-    </div>
+  <div class="flex-1 flex flex-col w-full items-center h-full justify-center overflow-hidden" data-cy="about">
+
+    <auth-form form-title="Login"/>
   </div>
 </template>
