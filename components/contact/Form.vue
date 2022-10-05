76f67e8f1f040134a2d4f994d546546d3b4f3947
M.Spinks <mikespinks0401@gmail.com>
2022-10-04 22:31:35 -0500

    formatting and update for form input component

--- components/contact/Form.vue
+++ components/contact/Form.vue
@@ -1,118 +1,130 @@
 <script setup lang="ts">
-  const name = ref('')
-  const email = ref('')
-  const phoneNumber = ref('')
+const name = ref('')
+const email = ref('')
+const phoneNumber = ref('')
 
-  const message = ref('')
+const message = ref('')
 
-  const showNameRequired = ref(false)
-  const showEmailRequired = ref(false)
-  const showMessageRequired = ref(false)
+const showNameRequired = ref(false)
+const showEmailRequired = ref(false)
+const showMessageRequired = ref(false)
 
-  const inputErrorsList = ref([])
-  const inputErrorsMsg = ref('')
+const inputErrorsList = ref([])
+const inputErrorsMsg = ref('')
 
-  const showAlert = ref(false)
+const showAlert = ref(false)
 
-  const requiredFieldClass =
-    'font-medium absolute -top-6 text-danger dark:text-dangerDark transition'
-  const updateName = (v: string) => {
-    name.value = v
-    if (showNameRequired.value === true && name.value !== '') {
-      showNameRequired.value = false
-    }
-    if (showNameRequired.value === false && name.value === '') {
-      showNameRequired.value = true
-    }
+const requiredFieldClass =
+  'font-medium absolute -top-6 text-danger dark:text-dangerDark transition'
+const updateName = (v: string) => {
+  name.value = v
+  if (showNameRequired.value === true && name.value !== '') {
+    showNameRequired.value = false
   }
+  if (showNameRequired.value === false && name.value === '') {
+    showNameRequired.value = true
+  }
+}
 
-  const updateEmail = (v: string) => {
-    email.value = v
-    if (showEmailRequired.value === true && email.value !== '') {
-      showEmailRequired.value = false
-    }
-    if (showEmailRequired.value === false && email.value === '') {
-      showEmailRequired.value = true
-    }
+const updateEmail = (v: string) => {
+  email.value = v
+  if (showEmailRequired.value === true && email.value !== '') {
+    showEmailRequired.value = false
   }
+  if (showEmailRequired.value === false && email.value === '') {
+    showEmailRequired.value = true
+  }
+}
 
-  const updatePhoneNumber = (v: string) => {
-    phoneNumber.value = v
-  }
+const updatePhoneNumber = (v: string) => {
+  phoneNumber.value = v
+}
 
-  const updateMessage = () => {
-    if (showMessageRequired && message.value !== '') {
-      showMessageRequired.value = false
-    }
-    if (showMessageRequired.value === false && message.value === '') {
-      showMessageRequired.value = true
-    }
+const updateMessage = () => {
+  if (showMessageRequired && message.value !== '') {
+    showMessageRequired.value = false
   }
+  if (showMessageRequired.value === false && message.value === '') {
+    showMessageRequired.value = true
+  }
+}
 
-  function submitContactForm() {
-    clearErrorData()
-    checkRequiredInputs()
-    alertIfErrors()
-    showAlert.value = true
+function submitContactForm() {
+  clearErrorData()
+  checkRequiredInputs()
+  alertIfErrors()
+  showAlert.value = true
+}
+function clearErrorData(): void {
+  inputErrorsList.value = []
+  inputErrorsMsg.value = ''
+}
+function checkRequiredInputs() {
+  if (name.value === '') {
+    showNameRequired.value = true
+    inputErrorsList.value.push('Name Required')
   }
-  function clearErrorData(): void {
-    inputErrorsList.value = []
-    inputErrorsMsg.value = ''
+  if (email.value === '') {
+    showEmailRequired.value = true
+    inputErrorsList.value.push('Email Required')
   }
-  function checkRequiredInputs() {
-    if (name.value === '') {
-      showNameRequired.value = true
-      inputErrorsList.value.push('Name Required')
-    }
-    if (email.value === '') {
-      showEmailRequired.value = true
-      inputErrorsList.value.push('Email Required')
-    }
-    if (message.value === '') {
-      showMessageRequired.value = true
-      inputErrorsList.value.push('Message Required')
-    }
+  if (message.value === '') {
+    showMessageRequired.value = true
+    inputErrorsList.value.push('Message Required')
   }
-  function alertIfErrors() {
-    if (
-      showNameRequired.value === true ||
-      showEmailRequired.value === true ||
-      showMessageRequired.value == true
-    ) {
-      let current = 0
-      for (let error of inputErrorsList.value) {
-        inputErrorsMsg.value += error
-        if (current + 1 === inputErrorsList.value.length) {
-          showAlert.value = true
-        } else {
-          inputErrorsMsg.value += '\n'
-          current++
-        }
+}
+function alertIfErrors() {
+  if (
+    showNameRequired.value === true ||
+    showEmailRequired.value === true ||
+    showMessageRequired.value == true
+  ) {
+    let current = 0
+    for (let error of inputErrorsList.value) {
+      inputErrorsMsg.value += error
+      if (current + 1 === inputErrorsList.value.length) {
+        showAlert.value = true
+      } else {
+        inputErrorsMsg.value += '\n'
+        current++
       }
     }
   }
+}
 </script>
 
 <template>
   <div class="w-full mb-8">
-    <teleport v-if="showAlert" to="#modal">
+    <teleport
+      v-if="showAlert"
+      to="#modal"
+    >
       <modals-alert
         @closeModal="showAlert = false"
         title="Alert"
         buttonText="Got It"
       >
-        <div v-for="(error, index) of inputErrorsList" :key="index">
+        <div
+          v-for="(error, index) of inputErrorsList"
+          :key="index"
+        >
           <p class="block text-center text-danger font-medium">{{ error }}</p>
         </div>
       </modals-alert>
     </teleport>
-    <form @submit.prevent autocomplete="off">
+    <form
+      @submit.prevent
+      autocomplete="off"
+    >
       <div class="z-30 grid md:grid-cols-2 gap-6 w-full mt-8">
         <div class="relative">
-          <p v-if="showNameRequired" :class="requiredFieldClass">
+          <p
+            v-if="showNameRequired"
+            :class="requiredFieldClass"
+          >
             Name Required*
           </p>
-          <forms-input
+          <form-component-input
             @update-text="updateName"
             classes="focus:border-primaryAccent2"
             fieldName="Name"
@@ -122,10 +134,13 @@
           />
         </div>
         <div class="relative">
-          <p v-if="showEmailRequired" :class="requiredFieldClass">
+          <p
+            v-if="showEmailRequired"
+            :class="requiredFieldClass"
+          >
             Email Required*
           </p>
-          <forms-input
+          <form-component-input
             @update-text="updateEmail"
             classes="focus:border-primaryAccent2"
             fieldName="Email"
@@ -135,7 +150,7 @@
           />
         </div>
         <div class="md:col-span-2">
-          <forms-input
+          <form-component-input
             @update-text="updatePhoneNumber"
             classes="focus:border-primaryAccent2"
             fieldName="Phone Number"
@@ -145,7 +160,10 @@
           />
         </div>
         <div class="md:col-span-2 relative">
-          <p v-if="showMessageRequired" :class="requiredFieldClass">
+          <p
+            v-if="showMessageRequired"
+            :class="requiredFieldClass"
+          >
             Message Required*
           </p>
           <textarea
@@ -168,8 +186,8 @@
   </div>
 </template>
 
-<style scoped>
-  form > input {
-    @apply focus:border-primaryAccent2;
-  }
+<style lang="postcss" scoped>
+form>input {
+  @apply focus:border-primaryAccent2;
+}
 </style>
