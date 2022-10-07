b34d69ccbef934a15e540ff65311f57f324848e6
M.Spinks <mikespinks0401@gmail.com>
2022-10-06 21:41:00 -0500

    reset db migration

--- prisma/migrations/20221007003938_/migration.sql
+++ prisma/migrations/20221007003938_/migration.sql
@@ -2,14 +2,13 @@
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `profilePic` VARCHAR(191) NULL,
+    `role` ENUM('ADMIN', 'USER') NOT NULL DEFAULT 'USER',
     `attempts` INTEGER NOT NULL DEFAULT 0,
     `lockedOut` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `User_email_key`(`email`),
-    UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
