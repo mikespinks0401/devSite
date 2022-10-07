c18b90498479ce5a3632e19e6037dd83bd018f98
M.Spinks <mikespinks0401@gmail.com>
2022-10-07 18:36:42 -0500

    added auth functions

--- server/db/user.ts
+++ server/db/user.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import * as bcrypt  from 'bcrypt'
 
 // the shape we want the new user to be
-export const UserSchema = z.object({
+export const registerUserSchema = z.object({
     email: z.string().email({ message: 'Invalid email address' }),
     password: z.string().min(7, { message: 'Password Must Be More Than 7 Characters Long' }).trim(),
     passwordConfirm: z.string()
@@ -11,6 +11,10 @@ export const UserSchema = z.object({
     refine(data => data.password === data.passwordConfirm,
     { message: 'Password Confirm Must Match Password' })
 
+export const loginUserSchema = z.object({
+    email: z.string().min(1),
+    password: z.string().min(1)
+})
 interface NewUser{
     email: string,
     password: string,
@@ -41,6 +45,32 @@ export const getUserByEmail = (email: string) => {
     return prisma.user.findUnique({
         where: {
             email: email
+        }
+    })
+}
+
+export const verifyUser = (password: string, hashedPassword) => {
+    return bcrypt.compareSync(password, hashedPassword)
+}
+
+export const updateUserPasswordAttempts = (email: string) => {
+    return prisma.user.update({
+        where: {
+            email: email
+        }, data: {
+            attempts:{
+                increment: 1
+            }
+        }
+    })
+}
+
+export const lockOutUser = (email: string) => {
+    return prisma.user.update({
+        where: {
+            email: email
+        }, data: {
+            lockedOut: true
         }
     })
 }
