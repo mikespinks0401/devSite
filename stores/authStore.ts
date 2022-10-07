d8522c05faefea94ff0e7c2a3bb8ca1b4a14d058
M.Spinks <mikespinks0401@gmail.com>
2022-10-07 18:37:57 -0500

    added login to auth store

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -1,11 +1,22 @@
 export const useAuthStore = defineStore('authStore', () => {
   const name = ref('John Doe')
   const admin = ref(true)
+  const accessToken = ref("")
 
   function updateName(newName: string){
     name.value = newName
   }
+
+  const login = async(email: string, password: string) => {
+
+      return await useAsyncData(
+        'login', 
+        ()=> $fetch('/api/v1/auth/login', {
+          method: 'POST', 
+          body: {email: email, password: password}}
+          ))
+  }
   
 
-  return { name, admin, updateName }
+  return { name, admin, updateName, login }
 })
