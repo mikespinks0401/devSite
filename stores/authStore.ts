d8b6d24210f2f15904cb6e39cb58ec77b243b984
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 18:30:32 -0500

    cleaned authStore login function

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -1,3 +1,4 @@
+
 export const useAuthStore = defineStore('authStore', () => {
   const name = ref('John Doe')
   const admin = ref(true)
@@ -7,15 +8,18 @@ export const useAuthStore = defineStore('authStore', () => {
     name.value = newName
   }
 
-  const login = async(email: string, password: string) => {
-
-      return await useAsyncData(
+  const login = async (email: string, password: string) => {
+      return useAsyncData(
         'login', 
         ()=> $fetch('/api/v1/auth/login', {
           method: 'POST', 
-          body: {email: email, password: password}}
-          ))
+          body: {email: email, password: password}
+          }), {initialCache:false},
+        )
+
   }
+    
+
   
 
   return { name, admin, updateName, login }
