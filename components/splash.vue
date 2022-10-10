6764cba5200aab734ef598b5009743b73310d0c3
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 13:58:01 -0500

    created splash screen component

---
+++ components/splash.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div class="w-full min-h-screen flex flex-col justify-center items-center">
+        <div class="animate-pulse">
+            <div class="flex flex-col items-center">
+            <img src="favicon.ico"/>
+            <p>Loading</p>
+            </div>
+        </div>
+    </div>
+</template>
