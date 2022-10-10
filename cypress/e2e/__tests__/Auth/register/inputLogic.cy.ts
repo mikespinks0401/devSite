5e3bb641148d4364174341b8572079bf6b6a4349
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 18:23:27 -0500

    Alert contains Required message

--- cypress/e2e/__tests__/Auth/register/inputLogic.cy.ts
+++ cypress/e2e/__tests__/Auth/register/inputLogic.cy.ts
@@ -7,12 +7,15 @@ describe('Input Errors Work', ()=>{
         cy.visit('/register')
         cy.get('[data-cy="auth form"]')
       })
+
       it('alert pops up when submitting empty form', ()=>{
-        cy.get('[data-cy="formSubmit"]').click()
+        cy.get('[data-cy="formSubmit"]').should('be.visible').click()
         cy.contains('Alert')
       })
-      it('alert contains empty input messages when submitted', ()=>{
-        cy.get('[data-cy="formSubmit"]')
+      it('alert contains required  when submitted', ()=>{
+        cy.get('[data-cy="formSubmit"]').should('be.visible').click().then(()=>{
+          cy.get('[data-cy="modal"]').contains('Required')
+        })
       })
     })
 })
