4b9df8a82500f227959e331a9960998292d81584
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 23:47:38 -0500

    + refresh_token_deleteMany

--- seeder/teardown.ts
+++ seeder/teardown.ts
@@ -4,6 +4,7 @@ import { prisma } from '.'
 export const clearUsers = async () => {
     const response = await prisma.user.findMany()
     if(response.length > 0){
+    await prisma.refreshToken.deleteMany()
     await prisma.password.deleteMany()
     await prisma.profile.deleteMany()
     await prisma.user.deleteMany()
