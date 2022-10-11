1880dc4fb8bacdb9d3842028990ef96c7ba3de5e
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 23:50:48 -0500

    add remember me parameter to authStore login

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -8,12 +8,12 @@ export const useAuthStore = defineStore('authStore', () => {
     name.value = newName
   }
 
-  const login = async (email: string, password: string) => {
+  const login = async (email: string, password: string, rememberMe: boolean = false) => {
       return useAsyncData(
         'login', 
         ()=> $fetch('/api/v1/auth/login', {
           method: 'POST', 
-          body: {email: email, password: password}
+          body: {email: email, password: password, rememberMe: rememberMe}
           }), {initialCache:false},
         )
 
