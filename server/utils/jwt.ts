ee18763190b9ee5189d45909e30902154ab47745
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 23:50:16 -0500

    added rememberMe parameter to create token

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import jwt_decode from 'jwt-decode'
 
 const generateAccessToken = (user) => {
 
@@ -7,10 +8,12 @@ const generateAccessToken = (user) => {
     return jwt.sign({userId: user.id}, config.jwtAccessSecret, {expiresIn: '5min'})
 }
 
-const generateRefreshToken = (user) => {
+const generateRefreshToken = (user, rememberMe = false) => {
+    //Set Token Expirty Time To 7 Days if remember me is checked
+    const expiresInTime = rememberMe === true ? '7d' : '-2000'
     const config = useRuntimeConfig()
 
-    return jwt.sign({user: user.id }, config.jwtRefreshSecret, {expiresIn: '3d'})
+    return jwt.sign({user: user.id}, config.jwtRefreshSecret, {expiresIn: expiresInTime})
 }
 
 export const decodeRefreshToken = (token) => {
@@ -23,13 +26,32 @@ export const decodeRefreshToken = (token) => {
     }
 }
 
-export const generateTokens = (user) => {
+export const generateTokens = (user, rememberMe:boolean) => {
     const accessToken = generateAccessToken(user)
-    const refreshToken = generateRefreshToken(user)
+    const refreshToken = generateRefreshToken(user, rememberMe)
 
     return {
         accessToken: accessToken,
         refreshToken: refreshToken
     }
+}
 
+
+export const sendRefreshToken = (event, token) => {
+    //decode token to get the time diff to set cookie 
+    const decoded = jwt_decode(token)
+    //add the maxAge value when the token is set to remember me
+    if(decoded.exp > decoded.iat){
+        const timeDiff = decoded.exp - decoded.iat
+        setCookie(event.res, 'refresh_token', token, {
+            maxAge: timeDiff,
+            httpOnly: true,
+            sameSite: true,
+        })
+    } else {
+        setCookie(event.res, 'refresh_token', token, {
+            httpOnly: true,
+            sameSite: true,
+        })
+    }
 }
