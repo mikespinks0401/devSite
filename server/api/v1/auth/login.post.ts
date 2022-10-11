3760af85d916ab3dcd9c8c9630f887630da017fc
M.Spinks <mikespinks0401@gmail.com>
2022-10-10 23:48:05 -0500

    added function to generate and send refreshTokens

--- server/api/v1/auth/login.post.ts
+++ server/api/v1/auth/login.post.ts
@@ -1,6 +1,9 @@
+import { sendRefreshToken } from './../../../utils/jwt';
 import { resetUserPasswordAttempts } from './../../../db/user';
 import { loginUserSchema, getUserByEmail, incrementUserPasswordAttempts, verifyUser, lockOutUser } from "~~/server/db/user"
 import { userTransformer } from '~~/server/transforms/users'
+import { generateTokens } from '~~/server/utils/jwt';
+import { createRefreshToken } from '~~/server/db/refreshTokens';
 
 
 export default defineEventHandler(async event => {
@@ -48,11 +51,21 @@ export default defineEventHandler(async event => {
         }))
         return
     }
-
+    //reset user login attempts
     await resetUserPasswordAttempts(user.id)
+
+    //generate tokens with user and remember Me option
+    const {accessToken, refreshToken} = generateTokens(user, body.rememberMe)
+
+    //add RefreshToken To Database
+    await createRefreshToken(refreshToken, user.id)
+
+
+    sendRefreshToken(event, refreshToken)    
     return {
         data: {
-        user: userTransformer(user)
+            accessToken: accessToken,
+            user: userTransformer(user)
         }
         }
     
