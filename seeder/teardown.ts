849c81e912a43a9084603e0c20ee3c47011a5cc7
M.Spinks <mikespinks0401@gmail.com>
2022-10-12 09:43:16 -0500

    make sure environment is set to 'development'

--- seeder/teardown.ts
+++ seeder/teardown.ts
@@ -2,6 +2,9 @@ import { prisma } from '.'
 
 
 export const clearUsers = async () => {
+    if(process.env.ENVIRONMENT !== 'development'){
+        return
+    }
     const response = await prisma.user.findMany()
     if(response.length > 0){
     await prisma.refreshToken.deleteMany()
