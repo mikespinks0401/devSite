96d7c2a315aed85464693407817443fc829f9e8e
M.Spinks <mikespinks0401@gmail.com>
2022-10-13 11:01:48 -0500

    +create tokens on success -> auto Login

--- server/api/v1/auth/register.post.ts
+++ server/api/v1/auth/register.post.ts
@@ -1,6 +1,8 @@
 import { userTransformer } from '~~/server/transforms/users'
+import { generateTokens, sendRefreshToken } from '~~/server/utils/jwt'
 import { H3Event } from 'h3'
 import { registerUserSchema, createUser, getUserByEmail } from '~~/server/db/user'
+import { createRefreshToken } from '~~/server/db/refreshTokens'
 
 export default defineEventHandler( async event => {  
     const body = await useBody(event)
@@ -27,16 +29,33 @@ export default defineEventHandler( async event => {
             statusMessage: 'Email Already Exists'
         }))
     }
-
+    //Put User into DB
     const user = await createUser(body)
+
+    //Create Tokens for user to be logged in
+    const {accessToken, refreshToken} = generateTokens(user)
+    try{
+        await createRefreshToken(refreshToken, user.id)
+        sendRefreshToken(event, refreshToken)
+    } catch(err) {
+        return sendError(event, createError({
+            statusCode: 500,
+            statusMessage: 'There was a server error'
+        }))
+    }
 
     event.res.statusCode = 201
 
     return {
-        data: userTransformer(user)
+        data:{
+            accessToken: accessToken,
+            user: userTransformer(user)
+        }
     }
     
 })
+
+
 
 function isBodyEmpty(event: H3Event, body){
     if(!body){
