bf2a22d2b741a5fc23f11d1a3644021deb6f9752
M.Spinks <mikespinks0401@gmail.com>
2022-10-14 14:59:10 -0500

    +Email interface +MessageDetails interface

--- server/utils/mail.ts
+++ server/utils/mail.ts
@@ -2,17 +2,38 @@ import nodemailer from 'nodemailer'
 import hbs from 'nodemailer-express-handlebars'
 import path from 'path'
 const user = process.env.EMAIL_EMAIL
-const pass = process.env.PASSWORD_PASSWORD
+const pass = process.env.EMAIL_PASSWORD
 const name = process.env.EMAIL_NAME
 
 const from = `"${name}" <${user}>`
 
-const dev = true;
-const host = dev === true ? 'localhost' : process.env.EMAIL_HOST
-const port = dev === true ? '7778' : process.env.EMAIL_PORT
+const testingServer = false
+const host = testingServer ? 'localhost' : process.env.EMAIL_HOST
+const port = testingServer ? '7778' : process.env.EMAIL_PORT
 
+interface MessageDetails{
+    emailAddress: string,
+    emailMsg: string,
+    emailName: string,
+    emailSubject: string,
+    emailPhone?: string
+}
+interface Email{
+    
+    to: string,
+    subject: string,
+    msg: MessageDetails,
+    template:string
+    
+}
 
-export const email = async (to, subject, msg) => {
+export const email:(msgInfo: Email) => Promise<void> = async (msgInfo: Email) => {
+    if(!msgInfo){
+        return
+    }
+    if(!msgInfo.template){
+        return
+    }
     let transporter = nodemailer.createTransport({
         host: host,
         port: port,
@@ -29,18 +50,28 @@ export const email = async (to, subject, msg) => {
         layoutsDir: path.resolve('./server/utils/email/layouts/'),
         partialsDir: path.resolve('./server/utils/email/partials/')
     },
-    viewPath: path.resolve('./server/utils/email/emailTemplates'),
+    viewPath: path.resolve('./server/utils/email/templates'),
 
 
     }, ))
 
+    if (msgInfo.template === 'contact'){
+
+    }
+
 
     let info = await transporter.sendMail({
         from: from,
-        to: to,
-        subject: "Formatted #2",
-        text: `${msg}`,
-        template:'index'
+        to: msgInfo.to,
+        subject: msgInfo.subject,
+        template:msgInfo.template,
+        context:{
+            emailAddress: msgInfo.msg.emailAddress,
+            emailMsg: msgInfo.msg.emailMsg,
+            emailName: msgInfo.msg.emailName,
+            emailSubject: msgInfo.msg.emailSubject,
+            emailPhone: msgInfo.msg.emailPhone
+        }
     })
 
     console.log("Message Sent: %s", info.messageId)
