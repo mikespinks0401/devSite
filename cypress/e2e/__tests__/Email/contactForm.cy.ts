11a14f1e20087cc9c1f6e5b860f62f35e0002202
M.Spinks <mikespinks0401@gmail.com>
2022-10-14 15:01:02 -0500

    test email functionality when submitted

--- cypress/e2e/__tests__/Email/contactForm.cy.ts
+++ cypress/e2e/__tests__/Email/contactForm.cy.ts
@@ -1,7 +1,13 @@
 /// <reference types="cypress" />
 
-describe('can send email', ()=>{
-    it('successfully sends email', ()=> {
-        
-    })
+describe('contact form sends email', ()=>{
+
+   it('sends email on submission', ()=> {
+       cy.visit('/contact')
+       cy.get('[data-cy="name"]').should('be.visible').type('john doe')
+       cy.get('[data-cy="email"]').type('sampleEmail@email.com')
+       cy.get('[data-cy="message"]').type('Hello, This is a sample Message')
+       cy.get('[data-cy="submit"]').click()
+       
+      })
 })
