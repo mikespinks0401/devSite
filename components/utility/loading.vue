7815913e2eedcaddf5d965e3a8b79ea0bf7d9370
M.Spinks <mikespinks0401@gmail.com>
2022-10-15 12:35:51 -0500

    +consume class with props values

--- components/utility/loading.vue
+++ components/utility/loading.vue
@@ -5,21 +5,19 @@ interface Props {
     width?: number,
 }
 
-const props = withDefaults(defineProps<Props>(),{
-    size: 3,
+const props = withDefaults(defineProps<Props>(), {
+    size: 4,
     color: 'gray-800',
     width: 2,
 
 })
 
-const useClass = `h-${props.size} w-${props.size} border-${props.color} border-t-${props.width}`
+const useClass = `h-${props.size} w-${props.size} animate-spin-loading rounded-lg border-t-${props.width}`
 
 </script>
 
 <template>
-    <div class=" h-3 w-3 animate-spin-loading rounded-lg border-t-2"
-    >
-    
-</div>
+    <div :class="useClass">
+
+    </div>
 </template>
-<!-- :class="useClass"     -->
