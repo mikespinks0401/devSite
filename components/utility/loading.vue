c2cd9eb1b357e340db181bf43253b9b606962745
M.Spinks <mikespinks0401@gmail.com>
2022-10-15 09:47:18 -0500

    loading utility component

---
+++ components/utility/loading.vue
@@ -0,0 +1,25 @@
+<script setup lang='ts'>
+interface Props {
+    size?: number,
+    color?: string
+    width?: number,
+}
+
+const props = withDefaults(defineProps<Props>(),{
+    size: 3,
+    color: 'gray-800',
+    width: 2,
+
+})
+
+const useClass = `h-${props.size} w-${props.size} border-${props.color} border-t-${props.width}`
+
+</script>
+
+<template>
+    <div class=" h-3 w-3 animate-spin-loading rounded-lg border-t-2"
+    >
+    
+</div>
+</template>
+<!-- :class="useClass"     -->
