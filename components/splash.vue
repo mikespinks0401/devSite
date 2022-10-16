194cb766c464c459091663be477bbdb74ee730b9
M.Spinks <mikespinks0401@gmail.com>
2022-10-16 15:12:06 -0500

    change path to Splash Img

--- components/splash.vue
+++ components/splash.vue
@@ -6,8 +6,8 @@
     <div class="absolute inset-0 flex flex-col justify-center items-center">
         <div class="animate-pulse">
             <div class="flex flex-col items-center">
-            <img src="favicon.ico"/>
-            <p>Loading</p>
+                <img src="../assets/logo.png" />
+                <p>Loading</p>
             </div>
         </div>
     </div>
