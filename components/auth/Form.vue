e13a70247bbcf2d145e66ab0b80ef4f2a5abf373
M.Spinks <mikespinks0401@gmail.com>
2022-10-16 15:12:59 -0500

    +token to reactive data +turnstile on auth forms

--- components/auth/Form.vue
+++ components/auth/Form.vue
@@ -6,11 +6,11 @@ const props = defineProps<{
   submitValue: string
 }>()
 
-const emits = defineEmits(['error', 'forgotPassword' ,'sendFormData'])
+const emits = defineEmits(['error', 'forgotPassword', 'sendFormData'])
 
 const loading = ref(false)
 
-watch(()=>props.loading, ()=>{
+watch(() => props.loading, () => {
   loading.value = props.loading
 })
 
@@ -18,7 +18,8 @@ const userData = reactive({
   email: '',
   password: '',
   passwordConfirm: '',
-  rememberMe: false
+  rememberMe: false,
+  token: null
 })
 
 const updateEmail = (v: string) => {
@@ -37,25 +38,29 @@ const submitForm = () => {
     const loginData = {
       email: userData.email,
       password: userData.password,
-      rememberMe: userData.rememberMe
+      rememberMe: userData.rememberMe,
+      token: userData.token
     }
     emits('sendFormData', loginData)
   } else {
     const loginData = {
       email: userData.email,
       password: userData.password,
-      passwordConfirm: userData.passwordConfirm
+      passwordConfirm: userData.passwordConfirm,
+      token: userData.token
     }
     emits('sendFormData', loginData)
   }
 }
 
+
+const turnStileRenderOptions = {theme: 'light'}
 const inputClasses = "border focus:border-color-primaryAccent2"
 </script>
 
 <template>
-  <div class="z-40 px-4 pb-8 border-2 bg-white w-full md:max-w-lg dark:text-black">
-    <div class="border-b-2 mb-8">
+  <div class="z-40 px-4 pb-8 border-2 bg-white w-full md:max-w-lg dark:text-black ">
+    <div class="border-b-2 mb-6">
       <h1 class=" text-center text-2xl  font-semibold p-4">{{props.formTitle}}</h1>
       <p
         v-if="props.formTitle !== 'Sign Up'"
@@ -99,7 +104,7 @@ const inputClasses = "border focus:border-color-primaryAccent2"
       </div>
       <div class="flex items-center">
         <div
-          v-if="props.formTitle === 'Login'"
+          v-if="props.formTitle === 'Log In'"
           class="flex w-full gap-2 relative -top-2"
         >
           <div class="flex-1 flex w-full items-center justify-between pt-2">
@@ -136,21 +141,32 @@ const inputClasses = "border focus:border-color-primaryAccent2"
             conditions.</p>
         </div>
       </div>
-      <div class="mt-2 flex justify-center bt-2 relative">
-
+      <div class="mt-2 flex flex-col justify-center border-t-2 relative">
+        <ClientOnly>
+          <div class="align-center my-3 flex justify-center">
+          <Turnstile 
+            :options="turnStileRenderOptions"
+            v-model="userData.token"
+          />
+          </div>
+        </ClientOnly>
         <button
           @click="submitForm"
+          type="submit"
           :disabled="loading"
           data-cy="formSubmit"
           class=" relative cursor-pointer bg-primaryAccent2 text-white font-semibold w-full py-2"
           aria-label="submit registration"
         >
-        <span class="relative">
-          <utility-loading v-if="loading === true" class="inline-block absolute -left-5 top-[4px]" />
-          {{props.submitValue}}
-        </span>
-      
-      </button>
+          <span class="relative">
+            <utility-loading
+              v-if="loading === true"
+              class="inline-block absolute -left-5 top-[4px]"
+            />
+            {{props.submitValue}}
+          </span>
+
+        </button>
 
       </div>
     </form>
